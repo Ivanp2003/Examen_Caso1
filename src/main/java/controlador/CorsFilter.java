933d823de19408ee23e67188b059b861008ef13e
Ivanp2003 <andrespanchichavez@gmail.com>
2026-02-11 10:35:28 -0500

    Actualizacion de Cors

--- src/main/java/controlador/CorsFilter.java
+++ src/main/java/controlador/CorsFilter.java
@@ -5,21 +5,25 @@ import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebFilter("/*") // Esto aplica el filtro a TODAS las rutas de la API
+@WebFilter("/*") // Aplica a todos los endpoints: login, estudiantes, materias, etc.
 public class CorsFilter implements Filter {
+    @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) 
             throws IOException, ServletException {
         
         HttpServletResponse response = (HttpServletResponse) res;
         
-        // Estos headers le dicen al navegador que confíe en React
+        // Permite que el puerto 3000 acceda a los recursos
         response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
         response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
         response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
         
         chain.doFilter(req, res);
     }
-    
-    public void init(FilterConfig filterConfig) {}
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {}
+
+    @Override
     public void destroy() {}
 }
